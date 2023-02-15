b9920e851610dd51db4b948dc534131d68cc74a1
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-02-14 20:39:15 -0600

    New features
    More dice, record tracker, roll tracker

--- App.jsx
+++ App.jsx
@@ -7,6 +7,10 @@ export default function App() {
 
     const [dice, setDice] = React.useState(allNewDice())
     const [tenzies, setTenzies] = React.useState(false)
+    const [count, setCount] = React.useState(0)
+    const [record, setRecord] = React.useState(
+        () => JSON.parse(localStorage.getItem("record")) || 0
+    )
 
     React.useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
@@ -14,8 +18,13 @@ export default function App() {
         const allSameValue = dice.every(die => die.value === firstValue)
         if (allHeld && allSameValue) {
             setTenzies(true)
+            count < record && setRecord(count)
         }
     }, [dice])
+
+    React.useEffect(() => {
+        localStorage.setItem("record", JSON.stringify(record))
+    }, [record])
 
     function generateNewDie() {
         return {
@@ -27,23 +36,25 @@ export default function App() {
 
     function allNewDice() {
         const newDice = []
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 12; i++) {
             newDice.push(generateNewDie())
         }
         return newDice
     }
 
     function rollDice() {
-        if (!tenzies) {
-            setDice(oldDice => oldDice.map(die => {
-                return die.isHeld ?
-                    die :
-                    generateNewDie()
-            }))
-        } else {
-            setTenzies(false)
-            setDice(allNewDice())
-        }
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ?
+                die :
+                generateNewDie()
+        }))
+        setCount(oldCount => oldCount + 1)
+    }
+
+    function newGame() {
+        setTenzies(false)
+        setDice(allNewDice())
+        setCount(0)
     }
 
     function holdDice(id) {
@@ -59,7 +70,7 @@ export default function App() {
             key={die.id}
             value={die.value}
             isHeld={die.isHeld}
-            holdDice={() => holdDice(die.id)}
+            holdDice={!tenzies && (() => holdDice(die.id))}
         />
     ))
 
@@ -69,12 +80,14 @@ export default function App() {
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same.
                 Click each die to freeze it at its current value between rolls.</p>
+            <h2>Record: {record}</h2>
+            <h2>Number of rolls: {count}</h2>
             <div className="dice-container">
                 {diceElements}
             </div>
             <button
                 className="roll-dice"
-                onClick={rollDice}
+                onClick={tenzies ? newGame : rollDice}
             >
                 {tenzies ? "New Game" : "Roll"}
             </button>
--- index.jsx
+++ index.jsx
@@ -3,9 +3,3 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />)
-
-// Things to add:
-//     Number of rolls
-//     Save record
-//     Time?
-// Learn JSON package
